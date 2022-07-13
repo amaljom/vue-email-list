9ebf8b94daec6d94dc71f066d4674668ca24cf5d
amaljom <amaljom99@gmail.com>
2022-07-13 18:13:56 +0200

    loading bar migliorare ma funziona, ora ha la width di deafult quind torna a 0 e poi alla lunghezza effettiva, ora lo provo a migliorare

--- js/script.js
+++ js/script.js
@@ -12,6 +12,22 @@ const app= new Vue(
         lenghtTester:function(){
             return this.emails.length>=10;
         },
+        loadingBarClass: function(){
+            switch (this.emails.length){
+                case 3:
+                    return 'w-25'
+                    break;
+                case 5:
+                    return 'w-50'
+                    break;
+                case 7:
+                    return 'w-75'
+                    break;
+                case 10:
+                    return 'w-10';
+                    break;
+            }
+        }
     },
     
 
